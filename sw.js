56ed41b5393bdbc3c86fd7e29354a02957d4c831
Jani <pikypapa@gmail.com>
2026-03-27 14:24:09 +0100

    Remove cache handling from service worker
    Removed cache handling and event listeners from the service worker.

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const CACHE_NAME = 'teszta-v4';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  // Mindig a hálózatról próbálja, ha nincs net, akkor a cache-ből
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+// Service Worker
