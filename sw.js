c81b1869db0ec3cd968f6b198dffaf56ffe83247
Jani <pikypapa@gmail.com>
2026-03-27 13:23:20 +0100

    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-// Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('teszta-store').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
