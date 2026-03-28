4df18a1297b4c6458f756adc0a6cd2758c7aaa28
Jani <pikypapa@gmail.com>
2026-03-28 08:24:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,33 @@
+const CACHE_NAME = 'teszta-master-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './1774606671886.png'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-  // Üres fetch handler, hogy telepíthető legyen PWA-ként
+  if (e.request.url.includes('firebaseio.com')) {
+    return fetch(e.request);
+  }
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
